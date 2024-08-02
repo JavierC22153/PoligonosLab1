7402793507bfe19356588b55610a36b85d8ad62c
Spark2570 <javier.chen50@gmail.com>
2024-08-01 21:47:46 -0600

    Dibuja el polígono 1 y el polígono 2 en azul con orilla blanca

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ fn main() {
     // Configuración de colores para los polígonos
     let color_white = Color::new(255, 255, 255);
     let color_yellow = Color::new(255, 255, 0);
+    let color_blue = Color::new(0, 0, 255);
 
     // Definición de polígonos
     let shape_1 = vec![
@@ -21,11 +22,21 @@ fn main() {
         [230, 360], [250, 380], [220, 385], [205, 410], [193, 383]
     ];
 
+    let shape_2 = vec![
+        [321, 335], [288, 286], [339, 251], [374, 302]
+    ];
+
     // Dibujar el primer polígono
     frame_buffer.set_draw_color(color_yellow); // Relleno
     frame_buffer.draw_filled_polygon(shape_1.clone());
     frame_buffer.set_draw_color(color_white); // Bordes en blanco
     frame_buffer.draw_polygon(shape_1.clone());
+
+    // Dibujar el segundo polígono
+    frame_buffer.set_draw_color(color_blue); // Relleno
+    frame_buffer.draw_filled_polygon(shape_2.clone());
+    frame_buffer.set_draw_color(color_white); // Bordes en blanco
+    frame_buffer.draw_polygon(shape_2.clone());
 
     // Guardar el buffer como un archivo BMP
     frame_buffer.save_as_bmp("output.bmp").unwrap();
